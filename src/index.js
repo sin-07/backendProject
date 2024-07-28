04b56c93b46127204634c7b3d3891b8aa8bfa3e6
Aniket singh <aniket.singh9322@gmail.com>
2024-07-28 13:51:44 +0530

    backendSetup

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+// (async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+//         app.on('error',(error)=>{
+//             console.log("ERR:",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is running on port ${process.env.PORT}`)
+//         })
+        
+//     }catch(err){
+//         console.log("ERROR:",err)
+//         throw err
+//     }
+// })
+    
