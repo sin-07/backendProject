e8d48e543bf3064534a5a1c2455796f138e8446f
Aniket singh <aniket.singh9322@gmail.com>
2024-07-29 14:44:54 +0530

    customeAPIresponse

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import dotenv from 'dotenv';
-import connectDB from './db/index.js';
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
-connectDB();
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on the port number ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+  });
 
 // (async ()=>{
 //     try{
@@ -25,10 +27,9 @@ connectDB();
 //         app.listen(process.env.PORT,()=>{
 //             console.log(`App is running on port ${process.env.PORT}`)
 //         })
-        
+
 //     }catch(err){
 //         console.log("ERROR:",err)
 //         throw err
 //     }
 // })
-    
